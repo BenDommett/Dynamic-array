a1ae9457b12ece90df869b8250b20d892282b490
ben <Bendomm@hotmail.co.uk>
2025-02-04 20:49:28 +0000

    test added for sorted array

--- src/test/java/ArraySortingTest.java
+++ src/test/java/ArraySortingTest.java
@@ -4,5 +4,21 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ArraySortingTest {
+    @Test
+    void integerSorttest(){
+        Array<Integer> arrayNum = new Array<>();
+        arrayNum.ensureCapacity(5);
+        arrayNum.add(4);
+        arrayNum.add(67);
+        arrayNum.add(3);
+        arrayNum.add(50);
+        arrayNum.add(28);
 
+
+        Integer[] nums = {3, 4 , 28 , 50 , 67};
+
+       assertArrayEquals(nums , arrayNum.sort());
+
+    }
 }
+
