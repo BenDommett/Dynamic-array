24b77d7c0e8f4016d3f2cbb5329c560b63b24e43
ben <Bendomm@hotmail.co.uk>
2025-02-06 00:40:40 +0000

    Tests added to SortingTest

--- src/test/java/ArraySortingTest.java
+++ src/test/java/ArraySortingTest.java
@@ -20,5 +20,49 @@ public class ArraySortingTest {
        assertArrayEquals(nums , arrayNum.sort());
 
     }
+
+
+    @Test
+    void StringSortTest(){
+        Array<String> names = new Array<>();
+        names.ensureCapacity(7);
+        names.add("James");
+        names.add("Ben");
+        names.add("Emma");
+        names.add("David");
+        names.add("Paul");
+        names.add("Dan");
+        names.add("Danny");
+
+
+        String[] namesSorted = {"Ben" , "Dan" ,"Danny", "David"  , "Emma" ,"James" , "Paul"};
+
+        assertArrayEquals(namesSorted , names.sort());
+
+    }
+
+    @Test
+    void DoubleSortTest(){
+        Array<Double> money = new Array<>();
+        money.ensureCapacity(8);
+        money.add(0.35);
+        money.add(20.35);
+        money.add(2.09);
+        money.add(12.5);
+        money.add(0.05);
+        money.add(3.5);
+        money.add(5.80);
+        money.add(1.78);
+
+        Double[] soortedMoney = {0.05, 0.35 ,1.78,2.09,3.5, 5.80,12.5,20.35};
+
+        assertArrayEquals(soortedMoney , money.sort());
+    }
+
+    @Test
+    void emptyArraySortTest(){
+
+    }
+
 }
 
